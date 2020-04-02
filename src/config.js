074a626ee43e65ebbf6e924cfa315bea79cf5be2
amantemiguel <61431200+amantemiguel@users.noreply.github.com>
2020-04-02 15:02:03 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_ZFLVAbyg0aXc1fOdPx16bWld00la9RXhj0",
   s3: {
-   REGION: "us-east-1",
-   BUCKET: "elbert-ballo-notes-app-uploads"
+    REGION: "us-east-1",
+    BUCKET: "elbert-ballo-notes-app-uploads"
   },
   apiGateway: {
-   REGION: "us-east-1",
-   URL: "https://hbzgm5jveh.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "us-east-1",
+    URL: "https://hbzgm5jveh.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-   REGION: "us-east-1",
-   USER_POOL_ID: "us-east-1_7vp3PZ8LD",
-   APP_CLIENT_ID: "210575m5oo96c6d9d7h819c0no",
-   IDENTITY_POOL_ID: "us-east-1:8e769d38-64dc-4188-9523-34f91137a473"
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_7vp3PZ8LD",
+    APP_CLIENT_ID: "210575m5oo96c6d9d7h819c0no",
+    IDENTITY_POOL_ID: "us-east-1:8e769d38-64dc-4188-9523-34f91137a473"
   }
- };
+};
